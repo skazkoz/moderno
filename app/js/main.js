0675bb7b93da80d8b193c1c354c6f493feaafc11
skazkoz <skazkoz@gmail.com>
2020-11-14 23:53:32 +0200

    added slider product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $(function() {
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+    $('.product-slider_inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
     var mixer = mixitup('.product_inner-box');
 
 
